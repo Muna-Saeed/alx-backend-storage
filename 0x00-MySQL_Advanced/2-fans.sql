629df108b9b65378a24092fdb0f6fcde86f3cdcf
Muna Saeed <munasaeed6367@gmail.com>
2024-05-09 15:39:18 -0700

    2. Best band ever!

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,11 @@
+-- Import the provided table dump file (metal_bands.sql.zip) into your database.
+-- This file contains the necessary data for the bands.
+-- Rank country origins of bands by number of fans
+-- Ordered by the number of (non-unique) fans
+-- Column must be origin and nb_fans
+-- Script can be executed on any database
+
+SELECT origin, COUNT(*) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
