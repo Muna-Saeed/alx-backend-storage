16d7fe348803b7bd0ad78336c6bd60c4df28c29b
Muna Saeed <munasaeed6367@gmail.com>
2024-05-09 16:10:59 -0700

    4. Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,13 @@
 -- Create a trigger that decreases the quantity of an item after adding a new order
 
-CREATE TRIGGER decrease_quantity_trigger
-AFTER INSERT ON orders
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE item_id = NEW.item_id;
-END;
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END//
+
+DELIMITER ;
