c1c84b9c56ad53b171e3ce6179962ec5bc57b225
Muna Saeed <munasaeed6367@gmail.com>
2024-05-15 15:27:47 -0700

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -47,6 +47,23 @@ def call_history(method: Callable) -> Callable:
 
     return wrapper
 
+def replay(method: Callable):
+    """
+    Display the history of calls of a particular function.
+    """
+    r = redis.Redis()
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+
+    inputs = r.lrange(input_key, 0, -1)
+    outputs = r.lrange(output_key, 0, -1)
+
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+    for input_data, output_data in zip(inputs, outputs):
+        input_str = input_data.decode('utf-8')
+        output_str = output_data.decode('utf-8')
+        print(f"{method.__qualname__}(*{input_str}) -> {output_str}")
+
 class Cache:
     """
     Cache class for interacting with Redis.
