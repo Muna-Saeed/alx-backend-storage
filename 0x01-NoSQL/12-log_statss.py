fe85a42bb4a74f4ece063601a37f8423345befda
Muna Saeed <munasaeed6367@gmail.com>
2024-05-17 10:50:50 -0700

    updated

--- 0x01-NoSQL/12-log_statss.py
+++ 0x01-NoSQL/12-log_statss.py
@@ -2,7 +2,10 @@
 """
 Provides stats about Nginx logs stored in MongoDB
 """
+
+
 from pymongo import MongoClient
+
 
 def log_stats():
     """
@@ -14,31 +17,20 @@ def log_stats():
 
     # Total number of logs
     total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+
+    print("{} logs".format(total_logs))
 
     # Count of different HTTP methods
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        print("    method {}: {}".format(method, count))
 
     # Number of documents with method=GET and path=/status
     status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check_count} status check")
+    print("{} status check".format(status_check_count))
 
-    # Top 10 most present IPs
-    pipeline = [
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ]
-    top_ips = collection.aggregate(pipeline)
-    print("IPs:")
-    for ip_info in top_ips:
-        ip = ip_info.get("_id")
-        count = ip_info.get("count")
-        print(f"    {ip}: {count}")
 
 if __name__ == "__main__":
     log_stats()
+
